bd9ec19a7c4f518d2badc309578dd5b0a753647f
Pratheek Shetty <pratheekshetty@Pratheeks-MBP.lan>
2026-09-24 12:59:02 +0530

    Adde first TS programme

---
+++ Day1/FirstDemo.js
@@ -0,0 +1,2 @@
+"use strict";
+console.log("welcome to tsc Day1/FirstDemo.ts");
---
+++ Day1/FirstDemo.ts
@@ -0,0 +1,4 @@
+console.log("welcome to tsc Day1/FirstDemo.ts +++");
+
+//tsc Day1/FirstDemo.ts
+//node Day1/FirstDemo.ts
